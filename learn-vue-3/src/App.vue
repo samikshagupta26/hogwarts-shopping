3b3559d31732c9dc8c342625184c805cc592ea5d
samikshagupta26 <gsamiksha2003@gmail.com>
2025-03-24 22:19:20 -0500

    vue named slots

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Hello, Vue!</div>
+  <BaseLayout>
+  <template #header>
+    <h1>Here might be a page title.</h1>
+  </template>
+  <!-- Anything without a template will be default or we can give it the #default title -->
+  <template #default>
+    <p>A paragraph of the main content.</p>
+    <p>And another one.</p>
+  </template>
+  <template #footer>
+    <p>Here's some contact info.</p>
+  </template>
+  </BaseLayout>
 </template>
 
 <script setup>
-
+import BaseLayout from './BaseLayout.vue';
 </script>
 
 <style lang="scss" scoped>
