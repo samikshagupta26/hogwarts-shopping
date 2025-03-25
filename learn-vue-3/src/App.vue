4d7db7cf2b8ad2b2b6195055bc356ed44561b599
samikshagupta26 <gsamiksha2003@gmail.com>
2025-03-24 22:16:16 -0500

    computed props in vue

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,23 +1,56 @@
 <template>
-  <!-- <ButtonCounter></ButtonCounter>
-  <ButtonCounter></ButtonCounter>
+  <!-- <BlogPost id="1" blogPostTitle="What is a muggle?" blogPostContent="A muggle is someone who lacks any kind of magical abilities"></BlogPost> -->
 
-  <button-counter></button-counter> -->
+  <!-- Good practice is using kebad case -->
+  <!-- <BlogPost id="1" blog-post-title="What is a muggle?" blog-post-content="A muggle is someone who lacks any kind of magical abilities"></BlogPost>
+  <BlogPost :id="blogPostId" :blog-post-title="blogPostTitle" :blog-post-content="blogPostContent"></BlogPost> -->
 
   <hr>
 
-  <div style="background-color: orange; padding: 5px;">
-    This is the root component.
-    <ComponentA></ComponentA>
-    <ComponentA></ComponentA>
-  </div>
+  <BlogPost v-for="post in posts" :id="post.id" :blog-post-title="post.blogPostTitle" :blog-post-content="post.blogPostContent" :key="post.id"></BlogPost>
+
+  <BlogPost v-for="post in posts" v-bind="post" :key="post.id"></BlogPost>
 </template>
 
 <script setup>
-import ButtonCounter from './ButtonCounter.vue';
-import ComponentA from '@/ComponentA.vue';
+import { ref } from 'vue';
+import BlogPost from './BlogPost.vue';
+
+let blogPostId = ref(2)
+let blogPostTitle = ref('HP and the Curse Child Broadway production suspended')
+let blogPostContent = ref(
+  'Adding to the earlier post, the Broadway production....'
+)
+
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
+  }
+])
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
